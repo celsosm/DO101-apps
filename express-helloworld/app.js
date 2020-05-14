fb5e73e01e9ab22c37846c45779b4988cdab9502
Celso Matins <celsonti@gmail.com>
2020-05-14 12:04:06 -0300

    add new version

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/home', function (req, res) {
   res.send('Hello World a new deploy!\n');
 });
 
+app.get('/versionini', function (req, res) {
+  res.send('Version 1.0.0\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
